b4242686f57f13c5ae73f15a3647308b372986db
nath-xdesign <nath.thoms33@gmail.com>
2022-12-02 16:41:16 +0000

    add toast package

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,13 @@
 import "../styles/globals.css"
 import Navbar from "../components/Navbar/Navbar"
-
-import { GetServerSideProps } from "next"
+import { Toaster } from "react-hot-toast"
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Navbar />
       <Component {...pageProps} />
+      <Toaster />
     </>
   )
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -2,13 +2,17 @@ import Head from "next/head"
 import Image from "next/image"
 import Link from "next/link"
 import styles from "../styles/Home.module.css"
+import toast from "react-hot-toast"
 
 import Loader from "../components/Loader/Loader"
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Loader show />
+      {/* <Loader show /> */}
+      <button onClick={() => toast.success("Some test toast")}>
+        Test toast
+      </button>
     </div>
   )
 }
