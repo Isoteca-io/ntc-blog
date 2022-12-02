38c27db22780879aea472e1705d9d1f0e9a61ca4
nath-xdesign <nath.thoms33@gmail.com>
2022-12-02 13:06:16 +0000

    firebase and TS setup

---
+++ lib/firebase.ts
@@ -0,0 +1,22 @@
+import firebase from "firebase/compat/app"
+import "firebase/compat/auth"
+import "firebase/compat/firestore"
+import "firebase/compat/storage"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCoUU3okU8RPzMFmdJub3quRkrOVIkNwVY",
+  authDomain: "ntc-blog.firebaseapp.com",
+  projectId: "ntc-blog",
+  storageBucket: "ntc-blog.appspot.com",
+  messagingSenderId: "582578084574",
+  appId: "1:582578084574:web:61103b527e521483a9004f",
+  measurementId: "G-YME189D8T8",
+}
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig)
+}
+
+export const auth = firebase.auth()
+export const firestore = firebase.firestore()
+export const storage = firebase.storage()
