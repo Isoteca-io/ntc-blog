216c579be5f465f3b196458f695f34fb61058ea9
nath-xdesign <nath.thoms33@gmail.com>
2022-12-02 13:33:34 +0000

    add routing and loader

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
 import Head from "next/head"
 import Image from "next/image"
+import Link from "next/link"
 import styles from "../styles/Home.module.css"
 
+import Loader from "../components/Loader/Loader"
+
 export default function Home() {
-  return <div className={styles.container}></div>
+  return (
+    <div className={styles.container}>
+      <Loader show />
+    </div>
+  )
 }
