191a31a16fb46e9275bdc8fe4bacd997d5656d4f
nath-xdesign <nath.thoms33@gmail.com>
2022-12-13 16:27:21 +0000

    added auth check to admin page

--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -1,10 +1,13 @@
 import Metatags from "../../components/Metatags/Metatags"
+import AuthCheck from "../../components/AuthCheck/AuthCheck"
 
-export default function AdminPostsPage({}) {
+export default function AdminPostsPage(props) {
   return (
     <main>
-      <Metatags title="Admin" description="Admin page" />
-      <h1>Admin</h1>
+      <AuthCheck>
+        <Metatags title="Admin" description="Admin page" />
+        <h1>Admin</h1>
+      </AuthCheck>
     </main>
   )
 }
